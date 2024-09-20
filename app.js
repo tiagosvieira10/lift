898dddd96ed6e1fdf4c14811e9bcc70571c4d187
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-09-20 13:48:15 -0300

    portfolio finalizado

--- app.js
+++ app.js
@@ -2,3 +2,21 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+const swiper = new Swiper('.slider-wrapper', {
+  loop: true,
+  grabCursor: true,
+  spaceBetween: 10,
+  slidesPerView: 1.25,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
