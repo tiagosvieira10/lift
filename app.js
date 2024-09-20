a27ff59554c886faed5f8cd8f90fef51a3a99d56
tiagosvieira10 <tiagosvieira10@gmail.com>
2024-09-20 18:51:13 -0300

    button funcionais

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
+// const hamburger = document.querySelector(".hamburger");
+// const nav = document.querySelector(".nav");
+
+// hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
 const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav");
+const navLinks = document.querySelectorAll(".nav-list a");
 
+// Abre/fecha o menu quando o botão hamburger é clicado
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+// Fecha o menu quando um link da nav-list é clicado
+navLinks.forEach(link => {
+  link.addEventListener("click", () => nav.classList.remove("active"));
+});
+
 
 const swiper = new Swiper('.slider-wrapper', {
   loop: true,
